974450685c71bc5c5563c3b400aee3eb5e6bd277
Rafael Tomelin <rafael.tomelin@gmail.com>
2017-06-21 20:11:01 -0300

    change rspec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,26 @@
 require 'spec_helper'
+
 describe 'spamassassin' do
-  context 'with default values for all parameters' do
-    it { should contain_class('spamassassin') }
-  end
+
+  let(:title) { 'spamassassin' }
+  let(:node) { 'test.example.com' }
+
+  it { is_expected.to compile }
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to contain_package('spamassassin').with(ensure: ‘present’) }
+
+  it { is_expected.to contain_file('/etc/mail/spamassassin/local.cf')
+    .with( 
+      :ensure => 'file',
+      :require => 'Package[nginx]', 
+     ) 
+  }
+
+
+  it { is_expected.to contain_service('spamassassin')
+    .with( 
+      :ensure => 'running',
+      :enabled => true,
+     ) 
+  }
 end
